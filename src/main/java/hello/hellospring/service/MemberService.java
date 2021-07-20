a72f05b47f2b0cea7b6dc941f33af145d3cc61cf
bellwest <bellwest93@gmail.com>
2021-07-21 02:07:26 +0900

    6. add service and tests for it

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    /**
+     * sign up
+     */
+    public Long join(Member member) {
+        // member name should be unique
+        validateDuplicateMember(member); // check if duplicate
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * lookup all members
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("already existing member");
+                });
+    }
+
+}
